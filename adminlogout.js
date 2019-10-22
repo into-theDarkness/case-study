725c6411dc9a3affb23fac363a21408bb1a20089
into-theDarkness <zzpeterpan0510@gmail.com>
2019-10-22 15:19:59 +0700

    hehe

--- adminlogout.js
+++ adminlogout.js
@@ -1,4 +1,4 @@
 function logout() {
     alert("Bạn đã đăng xuẩt");
-    window.location ="http://localhost:63342/case-study/login.html?_ijt=nubvfigo4ueko7f4u2su26e94e"
+    window.location ="./index.html"
 }
--- login.js
+++ login.js
@@ -8,16 +8,15 @@ function dangnhap() {
     for (let i =0;i<a.length;i++){
         if(taikhoan ===a[i]&& matkhau===b[i]){
             alert('Bạn đã đăng nhập với tư cách người dùng');
-            window.location = "http://localhost:63342/case-study/Case-study.html?_ijt=nf3g7h7os5bt3rb151nthqf7tg";
+            window.location = "./user.html";
             break;
         } else if (taikhoan===x[i] && matkhau===y[i]){
             alert('Bạn đã đăng nhập với tư cách admin');
-            window.location = "http://localhost:63342/case-study/Case-studyadmin.html?_ijt=hfoh1m120hs5ibn8v5t88siq57";
+            window.location = "./admin.html";
             break;
         } else {
             alert('Bạn đã nhập sai, vui lòng nhập lại');
-            window.location = "http://localhost:63342/case-study/login.html?_ijt=gsv3e5pbdrgr8r8g4r0los1n1n";
-            break;
+            window.location = "./index.html"
         }
     }
 }
