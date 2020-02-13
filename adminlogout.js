12ed9498b7b34fdd5dee5641fe391cc0a35a8931
into-theDarkness <zzpeterpan0510@gmail.com>
2020-02-13 22:33:12 +0700

    hehe

--- adminlogout.js
+++
@@ -1,4 +0,0 @@
-function logout() {
-    alert("Bạn đã đăng xuẩt");
-    window.location ="./index.html"
-}
--- mua.js
+++
@@ -1,3 +0,0 @@
-function mua() {
-    alert("Cảm ơn quý khách đã chọn mua hàng của chúng tôi");
-}
